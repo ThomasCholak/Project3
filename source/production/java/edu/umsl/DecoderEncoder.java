59116f593293133a4dbd1d9e32c96d32e760ef47
tbcwfb <tbcwfb@umsystem.edu>
2022-03-05 01:26:54 -0600

    second commit

--- source/production/java/edu/umsl/DecoderEncoder.java
+++ source/production/java/edu/umsl/DecoderEncoder.java
@@ -1,7 +1,15 @@
 package edu.umsl;
 
+import org.apache.commons.codec.binary.Base64;
+
+
 public class DecoderEncoder {
     public static void main(String[] args) {
-        System.out.println("Test");
+        String input = "SSBsb3ZlIG15IENNUCBTQ0kgMjI2MSBjbGFzcyBzbyBtdWNoLCBJIHdpc2ggSSBjb3VsZCBiZSBqdXN0IGFzIGNvb2wgYXMgbXkgcHJvZmVzc29y";
+        String decodedString = new String(decodeBase64(input));
+        System.out.println(decodedString);
+    }
+    private static byte[] decodeBase64(String base64String) {
+        return Base64.decodeBase64(base64String);
     }
 }
