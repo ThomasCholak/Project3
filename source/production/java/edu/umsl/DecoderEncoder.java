507495bb61a2dc6af09954d343443eae2ec33491
tbcwfb <tbcwfb@umsystem.edu>
2022-03-06 18:45:24 -0600

    final commit

--- source/production/java/edu/umsl/DecoderEncoder.java
+++ source/production/java/edu/umsl/DecoderEncoder.java
@@ -1,3 +1,12 @@
+/*
+Thomas Cholak
+Professor Steve Riegerix
+CMP SCI 2261
+6 March 2022
+
+Assignment: Decode the provided string and encode any test entered by the user with Base64 encryption
+ */
+
 package edu.umsl;
 
 import org.apache.commons.codec.binary.Base64;
@@ -5,18 +14,28 @@ import java.util.Scanner;
 
 public class DecoderEncoder {
     public static void main(String[] args) {
-        //part 1 -decoding the given string
-        String input = "SSBsb3ZlIG15IENNUCBTQ0kgMjI2MSBjbGFzcyBzbyBtdWNoLCBJIHdpc2ggSSBjb3VsZCBiZSBqdXN0IGFzIGNvb2wgYXMgbXkgcHJvZmVzc29y";
-        String readableString = new String(decodeBase64(input));
-        System.out.println(readableString);
+        boolean retry;
+        do {
+            try {
+                //part 1 -decoding the given string
+                String input = "SSBsb3ZlIG15IENNUCBTQ0kgMjI2MSBjbGFzcyBzbyBtdWNoLCBJIHdpc2ggSSBjb3VsZCBiZSBqdXN0IGFzIGNvb2wgYXMgbXkgcHJvZmVzc29y";
+                String readableString = new String(decodeBase64(input));
+                System.out.println(readableString);
 
-        //part 2 - encoding a string given by the user
-        Scanner reader = new Scanner(System.in); System.out.println();
-        System.out.println("Please input the text you would like to be encoded:");
-        String input2 = reader.nextLine();
-        byte[] newBytes = input2.getBytes(); //converts string to bytes
-        String readableString2 = new String(encodeBase64(newBytes)); //converts bytes to string
-        System.out.println(readableString2);
+                //part 2 - encoding a string given by the user
+                Scanner reader = new Scanner(System.in);
+                System.out.println();
+                System.out.println("Please input the text you would like to be encoded:");
+                String input2 = reader.nextLine();
+                byte[] newBytes = input2.getBytes(); //converts string to bytes for encryption
+
+                String readableString2 = new String(encodeBase64(newBytes)); //converts encrypted bytes to string
+                System.out.println(); System.out.println("Yor new string is: ");
+                System.out.print(readableString2); retry = false;
+
+            } catch (Exception ex) {
+                System.out.println("Error. Please retry."); retry = true; }
+        }  while (retry);
     }
     private static byte[] decodeBase64(String base64String) { return Base64.decodeBase64(base64String); }
     private static byte[] encodeBase64(byte[] binaryData) { return Base64.encodeBase64(binaryData); }
